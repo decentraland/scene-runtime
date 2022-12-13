1a1dbe6bdb3e567ad22cec24037e62fd77f22aaa
Leandro Mendoza <leandro@decentraland.org>
2022-12-13 11:42:16 -0300

    wip

--- src/common/web-worker-inject.js
+++ src/common/web-worker-inject.js
@@ -1,9 +1,10 @@
 // Until chrome fixes their virtual memory problem in which they allocate
 // 100GB(virt) to each WASM, we disable WASM in the scene workers
-globalThis.WebAssembly.Instance = function () {
-  throw new Error('Wasm is not allowed in scene runtimes')
+if (globalThis.WebAssembly) {
+  globalThis.WebAssembly.Instance = function () {
+    throw new Error('Wasm is not allowed in scene runtimes')
+  }
+  globalThis.WebAssembly.Module = function () {
+    throw new Error('Wasm is not allowed in scene runtimes')
+  }
 }
-globalThis.WebAssembly.Module = function () {
-  throw new Error('Wasm is not allowed in scene runtimes')
-}
-
